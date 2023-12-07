bf07a8ce5f0397555985548e165ee30455f39d34
Mohamed Majri <qwertyrioup@gmail.com>
2023-12-07 13:45:43 +0000

    last chance

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -14,13 +14,13 @@ const Navbar = () => {
         <span>Admin Dashboard</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="" className="icon" />
-        <img src="/app.svg" alt="" className="icon" />
-        <img src="/expand.svg" alt="" className="icon" />
-        <div className="notification">
+        {/* <img src="/search.svg" alt="" className="icon" /> */}
+        {/* <img src="/app.svg" alt="" className="icon" /> */}
+        {/* <img src="/expand.svg" alt="" className="icon" /> */}
+        {/* <div className="notification">
           <img src="/notifications.svg" alt="" />
           <span>1</span>
-        </div>
+        </div> */}
         <div className="user">
           <img
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
@@ -29,7 +29,7 @@ const Navbar = () => {
           {user && <span>{user.firstname} {user.lastname}</span> }
           
         </div>
-        <img src="/settings.svg" alt="" className="icon" />
+        {/* <img src="/settings.svg" alt="" className="icon" /> */}
         <img onClick={()=> {
           dispatch(logout())
         }} style={{width: 20, cursor: 'pointer' }} src="/logout.svg" alt="" className="icon" />
